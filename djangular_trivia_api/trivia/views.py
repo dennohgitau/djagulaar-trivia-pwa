02cadb10cede9e08e84287e94f1b24c822790f83
dennogitau <denisgitaustudy@gmail.com>
2022-11-23 09:13:34 +0300

    commit

---
+++ djangular_trivia_api/trivia/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
